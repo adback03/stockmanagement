e76f1e95af51f646928bf985a120d2c9ac56d29d
Andrew Backes <backes.andrew@gmail.com>
2014-02-24 19:57:04 -0600

    fix issue with redirects

--- StockManagementSystem/StockMarket/Default.aspx.cs
+++ StockManagementSystem/StockMarket/Default.aspx.cs
@@ -18,7 +18,7 @@ public partial class _Default : System.Web.UI.Page
         }
         else
         {
-            Response.Redirect(Request.ApplicationPath + "Login.aspx");
+            Response.Redirect("Login.aspx");
         }
 
         Settings.Initialize();
