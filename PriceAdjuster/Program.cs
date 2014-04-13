65cf535d4bc33ca4633987464d86bca33510c666
unknown <backes.andr@wngcsp04.intra.uwlax.edu>
2014-04-13 13:00:22 -0500

    More work on stats, and added new application to adjust prices

---
+++ PriceAdjuster/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseAccess;
+
+namespace PriceAdjuster
+{
+    class Program
+    {
+        private static string connStockMarket { get; set; }
+        private static string connSkyTrade { get; set; }
+
+        static void Main(string[] args)
+        {
+            connStockMarket = System.Configuration.ConfigurationManager.ConnectionStrings["StockMarket"].ToString();
+            connSkyTrade = System.Configuration.ConfigurationManager.ConnectionStrings["SkyTrade"].ToString();
+            DataTable dt = GetAllTickers();
+            foreach (DataRow dr in dt.Rows)
+            {
+                double currentPrice = GetPrice(dr["ticker"].ToString());
+                currentPrice += RandomDouble() * RandomPosOrNeg();
+                UpdatePrice(dr["ticker"].ToString(), currentPrice, connStockMarket);
+                UpdatePrice(dr["ticker"].ToString(), currentPrice, connSkyTrade);
+                //break;
+            }
+        }
+
+        private static double RandomDouble()
+        {
+            Random rand = new Random();
+            return rand.NextDouble();
+        }
+
+        private static int RandomPosOrNeg()
+        {
+            Random rand = new Random();
+            int i = rand.Next(100);
+
+            if (i % 2 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private static double GetPrice(string ticker)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT price FROM Stock WHERE ticker = '" + ticker + "'";
+            double currentPrice = double.Parse(SqlHelper.ExecuteScalar(cmd, connStockMarket));
+            return currentPrice;
+        }
+
+        /// <summary>
+        /// Get a list of all the activer tickers in Skytrade
+        /// </summary>
+        private static DataTable GetAllTickers()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT ticker FROM Stock";
+            return SqlHelper.ReturnAsTable(cmd, connStockMarket);
+        }
+
+        private static void UpdatePrice(string ticker, double price, string connection)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE Stock SET price = " + price + " WHERE ticker = '" + ticker + "'";
+            SqlHelper.ExecuteNonQuery(cmd, connection);
+        }
+    }
+}
--- StockManagementSystem/Skytrade/Controls/StatsControl.ascx.cs
+++ StockManagementSystem/Skytrade/Controls/StatsControl.ascx.cs
@@ -12,27 +12,40 @@ public partial class Controls_StatsControl : System.Web.UI.UserControl
     protected void Page_Load(object sender, EventArgs e)
     {
         if(!Page.IsPostBack){
-            LoadStocks();
-            GenerateQuickStats();
+            GenerateProfitMarginGraph();
         }
-        
     }
 
-    private void LoadStocks()
-    {
-        ddlStocks.DataSource = SkyTrade.GetAllTickers();
-        ddlStocks.DataTextField = "ticker";
-        ddlStocks.DataValueField = "ticker";
-        ddlStocks.DataBind();
-    }
+    //private void LoadStocks()
+    //{
+    //    chrtProfitMargin.Titles.Add("Stock");
+    //    foreach (DataRow row in SkyTrade.GetAllTickers().Rows)
+    //    {
+    //        DataTable dt = SkyTrade.GetProfitByStock(row["ticker"].ToString());
+    //        double spent = 0;
+    //        double earned = 0;
+    //        double profit = 0;
+    //        if (dt.Rows.Count > 0)
+    //        {
+    //            spent = double.Parse(dt.Rows[0]["Spent"].ToString());
+    //            earned = double.Parse(dt.Rows[0]["Earned"].ToString());
+    //            profit = double.Parse(dt.Rows[0]["Profit"].ToString());
+    //        }
 
-    private void GenerateQuickStats()
+    //        chrtProfitMargin.Series[0].Points.AddXY("Spent", spent);
+    //        chrtProfitMargin.Series[0].Points.AddXY("Earned", earned);
+    //        chrtProfitMargin.Series[0].Points.AddXY("Profit", profit);
+    //    }
+
+    //}
+
+    private void GenerateProfitMarginGraph()
     {
         GoogleChart chart = new GoogleChart();
         chart.elementId = "chart_div_byStock";
         chart.title = "Stocks Profit Margin ";
-        chart.width = 600;
-        chart.height = 600;
+        chart.width = 350;
+        chart.height = 350;
         chart.addColumn("string", "Stock Type");
         chart.addColumn("number", "Spent");
         chart.addColumn("number", "Earned");
